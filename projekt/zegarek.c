2be6f3da61fd5e0bf477893b484bfb738e57a71a
BestInTest <ziomal25565@gmail.com>
2025-12-12 12:22:41 +0100

    Odliczanie czasu

--- projekt/zegarek.c
+++ projekt/zegarek.c
@@ -56,22 +56,119 @@ void delay_timer(unsigned int x) {
     unsigned int i;
     for (i = 0; i < x; i++) {
         timer_flag = 0;
-        while(!timer_flag); // czekamy az poprzez przerwanie flaga ustawi się na 1
+        while(!timer_flag); // czekamy az poprzez przerwanie flaga ustawi si? na 1
+    }
+}
+
+unsigned char hours_l = 0; // l = left
+unsigned char hours_r = 0; // r - right
+unsigned char minutes_l = 0;
+unsigned char minutes_r = 0;
+unsigned char seconds_l = 0;
+unsigned char seconds_r = 0;
+
+void tick_clock() {
+    seconds_r++;
+
+    // liczenie sekund
+    if (seconds_r > 9) {
+        seconds_r = 0;
+        seconds_l++;
+    }
+
+    if (seconds_l >= 6) {
+        seconds_l = 0;
+        minutes_r++;
+    }
+
+    // liczenie minut
+    if (minutes_r > 9) {
+        minutes_r = 0;
+        minutes_l++;
+    }
+
+    if (minutes_l >= 6) {
+        minutes_l = 0;
+        hours_r++;
+    }
+
+
+    // liczenie godzin
+    if (hours_r > 9) {
+        hours_r = 0;
+        hours_l++;
+    }
+
+    if (hours_l >= 2 && hours_r >= 4) {
+        // resetowanie całego zegarka po 23:59:59
+        seconds_l = 0;
+        seconds_r = 0;
+        minutes_l = 0;
+        minutes_r = 0;
+        hours_l = 0;
+        hours_r = 0;
+    }
+}
+
+void update_time_on_segments() {
+    unsigned char i;
+    for (i = 0; i < DIGITS; i++) {
+        *csds = DIG_SELECT[i]; // wybieramy wyświetlacz
+
+        switch (i) {
+            case 0:
+                *csdb = NUMBERS[seconds_r]; // prawa strona - sekundy
+                break;
+            case 1:
+                *csdb = NUMBERS[seconds_l];
+                break;
+            case 2:
+                *csdb = NUMBERS[minutes_r]; // środek - minuty
+                break;
+            case 3:
+                *csdb = NUMBERS[minutes_l];
+                break;
+            case 4:
+                *csdb = NUMBERS[hours_r]; // lewa strona - godziny
+                break;
+            case 5:
+                *csdb = NUMBERS[hours_l];
+                break;
+        }
+
+        delay_timer(2); // krótkie opóźnienie dla multipleksowania
     }
 }
 
 void main() {
-
-    signed char seg_index = 0;
-    unsigned char iter;
+    unsigned int refresh_count = 0;
     DISP = 0;
     timer0_init();
 
     while (true) {
-        for (iter = 0; iter < DIGITS; iter++) {
-            *csds = DIG_SELECT[iter]; // wybieramy wyświetlacz
-            *csdb = NUMBERS[iter]; // wprowadzamy dane
-            delay_timer(100);
+        update_time_on_segments();
+
+        refresh_count++;
+        // Odświeżamy wyświetlacz ~100 razy na sekundę, co sekundę aktualizujemy czas
+        if (refresh_count >= 100) {
+            refresh_count = 0;
+            tick_clock();
         }
     }
 }
+/*
+void main() {
+
+    signed char seg_index = 0;
+    unsigned char iter;
+	DISP = 0;
+	timer0_init();
+
+	while (true) {
+	    for (iter = 0; iter < DIGITS; iter++) {
+            *csds = DIG_SELECT[iter]; // wybieramy wy?wietlacz
+            *csdb = NUMBERS[iter]; // wprowadzamy dane
+            delay_timer(1000);
+        }
+    }
+}*/
